ca6a5537c340d2b42c802bdeaf2d3b9114e56d89
Mubarik <burkatech2021@gmail.com>
2022-09-08 01:01:28 -0700

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+/**
+
+ * main - Prints the size of various types based on
+
+ *        the computer it is compiled and run on..
+
+ *
+
+ * Return: Always 0.
+
+ */
 int main(void)
   
 {
