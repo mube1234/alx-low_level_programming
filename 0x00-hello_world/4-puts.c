1d313fb7d83986e1cb36bd402eb2e1c611efdd78
Mubarik <burkatech2021@gmail.com>
2022-09-08 01:38:43 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,6 @@
  * Return: Always 0.
  */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0);  
 }
