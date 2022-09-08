d88456a256f51a677ac76b9715adb943823e2382
Mubarik <burkatech2021@gmail.com>
2022-09-08 01:05:26 -0700

    updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,28 @@
+
+
+/*
+
+ * File: 5-printf.c
+
+ */
+
+
+
+
 #include <stdio.h>
+
+/**
+
+ * main - Prints "with proper grammar, but the outcome is
+
+ *                a piece of art," followed by a new line.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
 int main(void)
   
 {
