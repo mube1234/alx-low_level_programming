c24b493f027510b26eea0b19cecb2da0e36b1743
Mubarik <burkatech2021@gmail.com>
2022-09-12 05:50:52 -0700

    updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - print the case
+*
+* description: using the main function
+* Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
+}
