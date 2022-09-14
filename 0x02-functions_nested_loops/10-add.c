65d7f649f44cab6052c31c2f5c33bb7052c80ada
Mubarik <burkatech2021@gmail.com>
2022-09-14 00:32:31 -0700

    updated

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,6 +3,9 @@
 /**
 * main - check the code
 *
+* add - Adds two integers.
+* @num1: The first integer to be added.
+* @num2: The second integer to be added.
 * Return: Always 0.
 */
 int add(int num1, int num2)
