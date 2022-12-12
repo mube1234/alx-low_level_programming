43ec47bd0f3073eeb2944652871a8c9a60a89f8c
Mubarik <burkatech2021@gmail.com>
2022-12-12 00:25:37 -0800

    updated

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,7 @@
-#ifndef LISTS_H
-#define LISTS_H
-#include <stdio.h>
+#ifndef _LISTS_H
+#define _LISTS_H
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -9,6 +9,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
+ * for ALX  project
  */
 typedef struct dlistint_s
 {
@@ -25,4 +26,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-#endif /* LISTS_H */
+#endif
